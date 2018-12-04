41304a611c21337e9ba44bccd025d7e563c8b2f8
Hiroaki OHSAWA <ohsawa@atmark-techno.com>
2018-12-04 16:48:16 +0900

    build mp+zp ot+zp but still not link at one

--- repo.sh
+++ repo.sh
@@ -4,4 +4,5 @@
 #ln -sr ../szephyr .
 git clone --depth 1 git://sv-scm/git/takumi.ando/ryukyu/zephyr.git -b degu_evk_20181122
 ln -sr ../openthread .
+git clone --depth 1 degu_20181127
 ln -sr ../micropython 
