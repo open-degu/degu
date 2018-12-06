84b6476feef8b97b105fb01c0d9b71f1d614001c
Hiroaki OHSAWA <ohsawa@atmark-techno.com>
2018-12-06 11:12:02 +0900

    update repo.sh

--- repo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#TODO: it mey "real" directory creation by repo
-#ln -sr ../szephyr .
-git clone --depth 1 git://sv-scm/git/takumi.ando/ryukyu/zephyr.git -b degu_evk_20181122
-ln -sr ../openthread .
-git clone --depth 1 degu_20181127
-ln -sr ../micropython 
