03f3a60c7cdadca95d53741721aec435d8648bd0
Keitaro TAKAHASHI <keitaro.takahashi@atmark-techno.com>
2019-11-29 10:00:49 +0900

    pm: Add timeout for waiting join when resume

--- degu_pm.c
+++ degu_pm.c
@@ -72,11 +72,23 @@ void openthread_suspend(otInstance *aInstance)
 
 void openthread_resume(otInstance *aInstance, uint8_t aChannel, otLinkModeConfig aConfig)
 {
+    s64_t uptime = k_uptime_get();
+    u32_t timeout = K_SECONDS(3);
+
+	u32_t elapsed = 0;
+
     otPlatRadioEnable(aInstance);
     otPlatRadioReceive(aInstance, aChannel);
     otThreadSetLinkMode(aInstance, aConfig);
     otIp6SetEnabled(aInstance, true);
     otThreadSetEnabled(aInstance, true);
+    k_usleep(200);
     while(otThreadGetDeviceRole(aInstance) <= OT_DEVICE_ROLE_DETACHED)
-    {}
+    {
+		elapsed += k_uptime_delta_32(&uptime);
+        if(elapsed >= timeout){
+            break;
+        }
+        k_usleep(USEC_PER_MSEC * 10U);
+    }
 }
