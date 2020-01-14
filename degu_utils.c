b72854159cb5bb8679b9ee3ad14a5c388bed94d4
Mitsuhiro Yoshida <mitsuhiro.yoshida@atmark-techno.com>
2020-01-14 11:35:14 +0900

    add se_hostlib

--- degu_utils.c
+++ degu_utils.c
@@ -30,7 +30,12 @@
 #include <stdio.h>
 #include <shell/shell.h>
 #include "zcoap.h"
-#include "degu_test.h"
+
+#define I2C
+#include "libA71CH_api.h"
+
+/* CAUTION: This is just a temoporary! */
+#define DEGU_TEST_TIMEOUT_SEC "300"
 
 extern char *net_byte_to_hex(char *ptr, u8_t byte, char base, bool pad);
 extern char *net_sprint_addr(sa_family_t af, const void *addr);
@@ -212,7 +217,7 @@ int degu_get_asset(void)
 {
 	char *key;
 	char *cert;
-	int  code;
+	int  code = 0;
 
 	key = k_malloc(2048);
 	cert = k_malloc(2048);
@@ -220,20 +225,31 @@ int degu_get_asset(void)
 	memset(key, 0, 2048);
 	memset(cert, 0, 2048);
 
-	/* At first, we must erase A71CH in here*/
+	if (LIBA71CH_open() != 0) {
+		goto end;
+	}
+
+	if (LIBA71CH_eraseKeyAndCert() != 0) {
+		goto a71ch_end;
+	}
 
 	code = degu_coap_request("x509/key", COAP_METHOD_GET, key, NULL);
 	if (code < COAP_RESPONSE_CODE_OK) {
-		goto end;
+		goto a71ch_end;
 	}
-	/* Write the key to A71CH in here */
 
 	code = degu_coap_request("x509/cert", COAP_METHOD_GET, cert, NULL);
 	if (code < COAP_RESPONSE_CODE_OK){
-		goto end;
+		goto a71ch_end;
 	}
-	/* Write the cert to A71CH in here */
 
+	if (LIBA71CH_setKeyAndCert(key, cert)) {
+		code = 0;
+		goto a71ch_end;
+	}
+
+a71ch_end:
+	LIBA71CH_finalize();
 end:
 	k_free(key);
 	k_free(cert);
@@ -250,7 +266,7 @@ int degu_send_asset(void)
 	char *key;
 	char *cert;
 	char timeout[4];
-	int  code;
+	int  code = 0;
 
 	key = k_malloc(4096);
 	cert = k_malloc(4096);
@@ -258,8 +274,17 @@ int degu_send_asset(void)
 	memset(key, 0, 4096);
 	memset(cert, 0, 4096);
 
-	strcpy(key, DEGU_TEST_KEY);
-	strcpy(cert, DEGU_TEST_CERT);
+	if (LIBA71CH_open() != 0) {
+		goto end;
+	}
+
+	if (LIBA71CH_getKeyAndCert(key, cert) != 0) {
+		LIBA71CH_finalize();
+		goto end;
+	}
+
+	LIBA71CH_finalize();
+
 	strcpy(timeout, DEGU_TEST_TIMEOUT_SEC);
 
 	code = degu_coap_request("con/key", COAP_METHOD_PUT, key, NULL);
